9ab7a46cac45cefb5e934a326235fe6ff020b1a4
Barak Rosner <rosnerbb@gmail.com>
2021-02-07 18:06:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,6 +85,7 @@ def adding_matz
     programmer_hash [:yukihiro_matsumoto] = {}
     programmer_hash [:yukihiro_matsumoto] [:know_for] = "Ruby"
     programmer_hash [:yukihiro_matsumoto] [:languages] = ["LISP", "C"]
+    programmer_hash
 
 end
 
